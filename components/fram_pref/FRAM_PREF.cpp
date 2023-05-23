f7b13e0adfc45cb0ce05d8b036b797d73c0c54cd
Aleksandar Dimitrov <admin@sharkydog.info>
2023-05-23 08:39:09 +0300

    fram_pref: add static preferences

--- components/fram_pref/FRAM_PREF.cpp
+++ components/fram_pref/FRAM_PREF.cpp
@@ -10,47 +10,89 @@ static const char * const TAG = "fram_pref";
 
 class FRAMPreferenceBackend : public ESPPreferenceBackend {
   public:
-    FRAMPreferenceBackend(fram::FRAM * fram, uint16_t addr, uint32_t type) {
-      fram_ = fram;
-      addr_ = addr;
+    FRAMPreferenceBackend(FRAM_PREF * comp, uint32_t type, uint8_t idx) {
+      comp_ = comp;
       type_ = type;
+      idx_ = idx;
     }
     
     bool save(const uint8_t *data, size_t len) override {
-      if (!fram_->isConnected()) {
+      if (!comp_->fram_->isConnected()) {
         return false;
       }
       
-      fram_->write(addr_, (uint8_t*)data, len);
-      fram_->write(addr_+len, (uint8_t*)&type_, 4);
+      auto & pref = comp_->prefs_[idx_];
+      
+      if( (pref.size_req-2) != (uint16_t)len ) {
+        return false;
+      }
+      
+      uint16_t checksum = checksum_((uint8_t*)data, len);
+      
+      comp_->fram_->write(pref.addr, (uint8_t*)data, len);
+      comp_->fram_->write(pref.addr+len, (uint8_t*)&checksum, 2);
       
       return true;
     }
     
     bool load(uint8_t *data, size_t len) override {
-      if (!fram_->isConnected()) {
-        return false;
-      }
-      if (type_ != fram_->read32(addr_+len)) {
+      if (!comp_->fram_->isConnected()) {
         return false;
       }
       
-      fram_->read(addr_, data, len);
+      auto & pref = comp_->prefs_[idx_];
       
+      if( (pref.size_req-2) != (uint16_t)len ) {
+        return false;
+      }
+      
+      std::vector<uint8_t> buff;
+      buff.resize(len);
+      comp_->fram_->read(pref.addr, buff.data(), len);
+      
+      if (checksum_(buff.data(), len) != comp_->fram_->read16(pref.addr+len)) {
+        return false;
+      }
+      
+      memcpy(data, buff.data(), len);
       return true;
     }
   
   protected:
-    fram::FRAM * fram_;
-    uint16_t addr_;
+    uint16_t checksum_(uint8_t *data, size_t len) {
+      uint16_t sum = (type_ >> 16) + (type_ & 0xFFFF);
+      
+      for (size_t i = 0; i < len; i++) {
+        sum += data[i];
+      }
+      
+      return sum;
+    }
+    
+    FRAM_PREF * comp_;
     uint32_t type_;
+    uint8_t idx_;
 };
 
-FRAM_PREF::FRAM_PREF(fram::FRAM * fram, uint16_t pool_size, uint16_t pool_start=0) {
+FRAM_PREF::FRAM_PREF(fram::FRAM * fram) {
   fram_ = fram;
+}
+
+void FRAM_PREF::set_pool(uint16_t pool_size, uint16_t pool_start=0) {
   pool_size_ = pool_size;
   pool_start_ = pool_start;
   pool_next_ = pool_start + 4;
+}
+
+void FRAM_PREF::set_static_pref(std::string key, uint16_t addr, uint16_t size, std::function<uint32_t()> && fn, bool persist_key) {
+  uint8_t flags = FLAG_STATIC;
+  
+  if (persist_key) {
+    flags |= FLAG_PERSIST_KEY;
+  }
+  
+  prefs_.push_back({.key=key, .addr=addr, .size=size, .size_req=0, .flags=flags});
+  prefs_static_cb_.push_back(fn);
 }
 
 void FRAM_PREF::setup() {
@@ -59,11 +101,30 @@ void FRAM_PREF::setup() {
     return;
   }
   
-  uint32_t hash = fnv1_hash(App.get_compilation_time());
-  if (hash != fram_->read32(pool_start_)) {
-    _clear();
-    fram_->write32(pool_start_, hash);
-    pool_cleared_ = true;
+  uint16_t fram_size = fram_->getSizeBytes();
+  size_t v_size = prefs_.size();
+  
+  for (size_t i = 0; i < v_size; i++) {
+    auto & pref = prefs_[i];
+    uint16_t addr_end = pref.size ? (pref.addr + pref.size - 1) : 0;
+    
+    if (fram_size && (addr_end >= fram_size)) {
+      pref.flags |= FLAG_ERR|FLAG_ERR_SIZE_FRAM;
+    }
+    
+    prefs_static_map_.insert({(prefs_static_cb_[i])(), i});
+  }
+  
+  prefs_static_cb_.clear();
+  
+  if (pool_size_) {
+    uint32_t hash = fnv1_hash(App.get_compilation_time());
+    
+    if (hash != fram_->read32(pool_start_)) {
+      _clear();
+      fram_->write32(pool_start_, hash);
+      pool_cleared_ = true;
+    }
   }
   
   pref_prev_ = global_preferences;
@@ -71,31 +132,78 @@ void FRAM_PREF::setup() {
 }
 
 void FRAM_PREF::dump_config() {
+  uint16_t fram_size = fram_->getSizeBytes();
+  
   ESP_LOGCONFIG(TAG, "FRAM_PREF:");
-  ESP_LOGCONFIG(TAG, "  Pool: %u bytes (%u-%u)", pool_size_, pool_start_, (pool_start_+pool_size_-1));
   
   if (!_check()) {
     return;
   }
   
-  if (pool_cleared_) {
-    ESP_LOGD(TAG, "  Pool was cleared");
+  if (pool_size_) {
+    uint16_t pool_end = pool_start_ + pool_size_;
+    
+    ESP_LOGCONFIG(TAG, "  Pool: %u bytes (%u-%u)", pool_size_, pool_start_, (pool_end-1));
+    if (pool_end > fram_size) {
+      ESP_LOGE(TAG, "  * Does not fit in FRAM (0-%u)!", (fram_size-1));
+    }
+    
+    if (pool_cleared_) {
+      ESP_LOGI(TAG, "  Pool was cleared");
+    }
+    
+    ESP_LOGCONFIG(TAG, "  Pool: %u bytes used", pool_next_ - pool_start_);
   }
-  ESP_LOGCONFIG(TAG, "  Pool: %u bytes used", pool_next_ - pool_start_ - 4);
+  
+  for (auto & pref : prefs_) {
+    std::string msg = str_sprintf("  Pref: key: %s", pref.key.c_str());
+    
+    if (pref.flags & FLAG_STATIC) {
+      //msg += ", STATIC";
+      
+      if (pref.flags & FLAG_PERSIST_KEY) {
+        msg += ", persist_key";
+      }
+    }
+    
+    if (pref.size) {
+      msg += str_sprintf(", addr: %u-%u", pref.addr, (pref.addr + pref.size - 1));
+    }
+    if (pref.size_req) {
+      msg += str_sprintf(", request size: %u", pref.size_req);
+    }
+    
+    if (!pref.size) {
+      //msg += ", IGNORE";
+      ESP_LOGW(TAG, "%s", msg.c_str());
+    }
+    else if (pref.flags & FLAG_ERR) {
+      ESP_LOGE(TAG, "%s", msg.c_str());
+      
+      if (pref.flags & FLAG_ERR_SIZE_REQ) {
+        ESP_LOGE(TAG, "  * Requested larger size!");
+      }
+      if (pref.flags & FLAG_ERR_SIZE_FRAM) {
+        ESP_LOGE(TAG, "  * Does not fit in FRAM (0-%u)!", (fram_size-1));
+      }
+      if (pref.flags & FLAG_ERR_SIZE_POOL) {
+        ESP_LOGE(TAG, "  * Does not fit in pool!");
+      }
+    }
+    else {
+      ESP_LOGD(TAG, "%s", msg.c_str());
+    }
+  }
 }
 
 bool FRAM_PREF::_check() {
   uint16_t fram_size = fram_->getSizeBytes();
-  uint16_t pool_end = pool_start_ + pool_size_;
   
   if (!fram_size) {
     ESP_LOGE(TAG, "  Device returns 0 size!");
     return false;
   }
-  if (pool_end > fram_size) {
-    ESP_LOGE(TAG, "  Pool (%u-%u) does not fit in FRAM (0-%u)!", pool_start_, (pool_end-1), (fram_size-1));
-    return false;
-  }
+  
   if (!fram_->isConnected()) {
     ESP_LOGE(TAG, "  Device connect failed!");
     return false;
@@ -105,6 +213,10 @@ bool FRAM_PREF::_check() {
 }
 
 void FRAM_PREF::_clear() {
+  if (!pool_size_) {
+    return;
+  }
+  
   uint8_t buff[16];
   uint16_t pool_end = pool_start_ + pool_size_;
   
@@ -126,15 +238,55 @@ ESPPreferenceObject FRAM_PREF::make_preference(size_t length, uint32_t type) {
     return {};
   }
   
-  uint16_t pool_end = pool_start_ + pool_size_;
-  uint16_t next = pool_next_ + length + 4;
+  auto pref_static_it = prefs_static_map_.find(type);
+  uint16_t size = (uint16_t)length + 2;
+  uint16_t addr;
+  uint8_t idx;
   
-  if (next > pool_end) {
-    return {};
+  if(pref_static_it != prefs_static_map_.end()) {
+    idx = pref_static_it->second;
+    auto & pref = prefs_[idx];
+    
+    pref.size_req = size;
+    
+    if (!pref.size) {
+      return {};
+    }
+    if (pref.size < size) {
+      pref.flags |= FLAG_ERR|FLAG_ERR_SIZE_REQ;
+      return {};
+    }
+    
+    addr = pref.addr;
+    
+    if (pref.flags & FLAG_PERSIST_KEY) {
+      type = fnv1_hash(pref.key);
+    }
+  }
+  else {
+    prefs_.push_back({.key=std::to_string(type), .addr=0, .size=0, .size_req=size, .flags=0});
+    idx = prefs_.size() - 1;
+    
+    if(!pool_size_) {
+      return {};
+    }
+    
+    uint16_t pool_end = pool_start_ + pool_size_;
+    uint16_t next = pool_next_ + size;
+    addr = pool_next_;
+    
+    prefs_[idx].addr = addr;
+    prefs_[idx].size = size;
+    
+    if (next > pool_end) {
+      prefs_[idx].flags |= FLAG_ERR|FLAG_ERR_SIZE_POOL;
+      return {};
+    }
+    
+    pool_next_ = next;
   }
   
-  auto *pref = new FRAMPreferenceBackend(fram_, pool_next_, type);
-  pool_next_ = next;
+  auto * pref = new FRAMPreferenceBackend(this, type, idx);
   
   return {pref};
 }
@@ -144,7 +296,9 @@ bool FRAM_PREF::sync() {
 }
 
 bool FRAM_PREF::reset() {
-  fram_->write32(pool_start_, 0);
+  if (pool_size_) {
+    fram_->write32(pool_start_, 0);
+  }
   return pref_prev_->reset();
 }
 
--- components/fram_pref/FRAM_PREF.h
+++ components/fram_pref/FRAM_PREF.h
@@ -3,13 +3,34 @@
 #include "esphome/core/component.h"
 #include "esphome/core/preferences.h"
 #include "esphome/components/fram/FRAM.h"
+#include <map>
 
 namespace esphome {
 namespace fram_pref {
 
+enum Flags : uint8_t {
+  FLAG_STATIC        = 0b00000001,
+  FLAG_PERSIST_KEY   = 0b00000010,
+  FLAG_ERR           = 0b10000000,
+  FLAG_ERR_SIZE_REQ  = 0b00010000,
+  FLAG_ERR_SIZE_FRAM = 0b00100000,
+  FLAG_ERR_SIZE_POOL = 0b01000000
+};
+
+struct PREF_STRUCT {
+  std::string key;
+  uint16_t addr;
+  uint16_t size;
+  uint16_t size_req;
+  uint8_t flags;
+};
+
 class FRAM_PREF : public Component, public ESPPreferences {
   public:
-    FRAM_PREF(fram::FRAM * fram, uint16_t pool_size, uint16_t pool_start);
+    FRAM_PREF(fram::FRAM * fram);
+    
+    void set_pool(uint16_t pool_size, uint16_t pool_start);
+    void set_static_pref(std::string key, uint16_t addr, uint16_t size, std::function<uint32_t()> && fn, bool persist_key);
     
     void setup() override;
     void dump_config() override;
@@ -21,14 +42,21 @@ class FRAM_PREF : public Component, public ESPPreferences {
     bool reset() override;
   
   protected:
+    friend class FRAMPreferenceBackend;
+    
     bool _check();
     void _clear();
     
     fram::FRAM * fram_;
-    uint16_t pool_size_;
-    uint16_t pool_start_;
-    uint16_t pool_next_;
+    uint16_t pool_size_{0};
+    uint16_t pool_start_{0};
+    uint16_t pool_next_{0};
     bool pool_cleared_{false};
+    
+    std::vector<PREF_STRUCT> prefs_;
+    std::vector<std::function<uint32_t()>> prefs_static_cb_;
+    std::map<uint32_t,uint8_t> prefs_static_map_;
+    
     ESPPreferences * pref_prev_;
 };
 
